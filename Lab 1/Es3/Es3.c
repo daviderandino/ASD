e331b42810e8a4eb13b510b1f01754d8398d6c7f
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-10-08 14:13:26 +0200

    Add files via upload

--- Lab 1/Es3/Es3.c
+++ Lab 1/Es3/Es3.c
@@ -18,6 +18,7 @@ void stampa_contenuti(struct corse *lc);
 void ordina_vett_by_data(struct corse *lc);
 void ordina_vett_by_partenza(struct corse *lc);
 void ordina_vett_by_arrivo(struct corse *lc);
+void ordina_vett_by_tratta(struct corse *lc);
 void ricerca_tratta_lineare(struct corse *lc);
 void ricerca_tratta_dicotomica(struct corse *lc);
 
@@ -34,7 +35,8 @@ void mostra_menu(struct corse *lc){
     printf("2.Ordina vettore per data\n");
     printf("3.Ordina vettore per stazione di partenza\n");
     printf("4.Ordina vettore per stazione di arrivo\n");
-    printf("5.Ricerca di una tratta per stazione di partenza\n");
+    printf("5.Ordina vettore per codice di tratta\n");
+    printf("6.Ricerca di una tratta per stazione di partenza\n");
     scanf("%d",&choice);
     switch (choice) {
         case 1:
@@ -50,7 +52,10 @@ void mostra_menu(struct corse *lc){
             ordina_vett_by_arrivo(lc);
             break;
         case 5:
-            ricerca_tratta_lineare(lc);
+            ordina_vett_by_tratta(lc);
+            break;
+        case 6:
+            ricerca_tratta_dicotomica(lc);
             break;
         default:
             exit(0);
@@ -61,20 +66,63 @@ void mostra_menu(struct corse *lc){
 
 void ricerca_tratta_lineare(struct corse *lc){
     char partenza[LENMAX];
-    int flag;
     printf("Inserisci la stazione da cercare:\n");
     scanf("%s",partenza);
     if(islower(partenza[0]))
         partenza[0] = partenza[0] - 32;
     for(int i=0;i<n_corse;i++){
-        flag = 1;
-        for(int j=0;j< strlen(partenza);j++){
-            if(partenza[j]!=(lc[i].partenza)[j])
-                flag = 0;
-        }
-        if(flag==1)
+        if(strncmp(lc[i].partenza,partenza, strlen(partenza))==0)
             printf("%s %s %s %s %s %s %d\n",lc[i].cod_tratta,lc[i].partenza,lc[i].destinazione,lc[i].data,lc[i].ora_part,lc[i].ora_arr,lc[i].ritardo);
     }
+}
+
+void ricerca_tratta_dicotomica(struct corse *lc) {
+    char partenza[LENMAX];
+    int m,k,end=0;
+    printf("Inserisci la tratta da cercare: \n");
+    scanf("%s", partenza);
+    if(islower(partenza[0]))
+        partenza[0] = partenza[0] - ('a' - 'A');
+    ordina_vett_by_partenza(lc);
+    int start_index = 0, end_index = n_corse - 1;
+    while (start_index <= end_index && end==0) {
+        m = start_index + (end_index - start_index) / 2;
+        if (strncmp(lc[m].partenza, partenza, strlen(partenza)) == 0) {
+            printf("%s %s %s %s %s %s %d\n", lc[m].cod_tratta, lc[m].partenza, lc[m].destinazione, lc[m].data,lc[m].ora_part, lc[m].ora_arr, lc[m].ritardo);
+            k = m + 1;
+            while(k<n_corse && strncmp(lc[k].partenza, partenza, strlen(partenza)) == 0){
+                printf("%s %s %s %s %s %s %d\n", lc[k].cod_tratta, lc[k].partenza, lc[k].destinazione, lc[k].data, lc[k].ora_part, lc[k].ora_arr, lc[k].ritardo);
+                k++;
+            }
+            k = m - 1;
+            while(k>0 && strncmp(lc[k].partenza, partenza, strlen(partenza)) == 0 && k>0){
+                printf("%s %s %s %s %s %s %d\n", lc[k].cod_tratta, lc[k].partenza, lc[k].destinazione, lc[k].data, lc[k].ora_part, lc[k].ora_arr, lc[k].ritardo);
+                k--;
+            }
+            end = 1;
+        }
+        else if (strncmp(lc[m].partenza, partenza, strlen(partenza)) < 0)
+            start_index = m + 1;
+        else
+            end_index = m - 1;
+    }
+}
+
+void ordina_vett_by_tratta(struct corse *lc){
+    int i, j, l=0, r=n_corse-1, flag = 1;
+    struct corse temp;
+    for(i = l;i<r && flag==1;i++){
+        flag = 0;
+        for(j = l; j < r - i + l; j++){
+            if(strcmp(lc[j].cod_tratta,lc[j+1].cod_tratta)>0){
+                flag = 1;
+                temp = lc[j];
+                lc[j] = lc[j+1];
+                lc[j+1] = temp;
+            }
+        }
+    }
+    printf("Vettore ordinato con successo in ordine di codice di tratta\n");
 }
 
 void ordina_vett_by_arrivo(struct corse *lc){
@@ -91,6 +139,7 @@ void ordina_vett_by_arrivo(struct corse *lc){
             }
         }
     }
+    printf("Vettore ordinato con successo in ordine di stazione di destinazione\n");
 }
 
 void ordina_vett_by_partenza(struct corse *lc){
@@ -107,6 +156,7 @@ void ordina_vett_by_partenza(struct corse *lc){
             }
         }
     }
+    printf("Vettore ordinato con successo in ordine di stazione di partenza\n");
 }
 
 void ordina_vett_by_data(struct corse *lc){
@@ -129,6 +179,7 @@ void ordina_vett_by_data(struct corse *lc){
             }
         }
     }
+    printf("Vettore ordinato con successo in ordine di data\n");
 }
 
 void stampa_contenuti(struct corse *lc){
