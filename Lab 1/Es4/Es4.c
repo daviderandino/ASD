8bfa307ecea738380f74cb7c322813d6c2b2500f
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-10-08 14:13:59 +0200

    Add files via upload

--- Lab 1/Es4/Es4.c
+++ Lab 1/Es4/Es4.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+#include<stdlib.h>
 #define MAXRIGHE 1000
 #define LENMAX 30
 
@@ -14,10 +15,11 @@ int n_corse;
 void riempi_vett(struct corse *lc);
 void stampa_contenuti(struct corse *lc);
 void ricerca_tratta_lineare(struct corse *lc);
-void ricerca_tratta_dicotomica(struct corse *lc);
+void ricerca_tratta_dicotomica(struct corse **rif_part);
 void ordina_vett_pointer_by_data(struct corse **rif_data);
 void ordina_vett_pointer_by_partenza(struct corse **rif_part);
 void ordina_vett_pointer_by_arrivo(struct corse **rif_arr);
+void ordina_vett_pointer_by_tratta(struct corse **rif_tratta);
 
 int main(){
     int choice=-1;
@@ -26,10 +28,12 @@ int main(){
     struct corse *rif_data[MAXRIGHE];
     struct corse *rif_partenze[MAXRIGHE];
     struct corse *rif_arrivi[MAXRIGHE];
+    struct corse *rif_tratta[MAXRIGHE];
     for(int i=0;i<n_corse;i++){
         rif_data[i] = &lista_corse[i];
         rif_partenze[i] = &lista_corse[i];
         rif_arrivi[i] = &lista_corse[i];
+        rif_tratta[i] = &lista_corse[i];
     }
     do{
         printf("Cosa vuoi fare? (0 per terminare)\n");
@@ -37,7 +41,8 @@ int main(){
         printf("2.Ordina vettore per data\n");
         printf("3.Ordina vettore per stazione di partenza\n");
         printf("4.Ordina vettore per stazione di arrivo\n");
-        printf("5.Ricerca di una tratta per stazione di partenza\n");
+        printf("5.Ordina vettore per codice di tratta\n");
+        printf("6.Ricerca di una tratta per stazione di partenza\n");
         scanf("%d",&choice);
         switch (choice) {
             case 1:
@@ -53,11 +58,33 @@ int main(){
                 ordina_vett_pointer_by_arrivo(rif_arrivi);
                 break;
             case 5:
-                ricerca_tratta_lineare(lista_corse);
+                ordina_vett_pointer_by_tratta(rif_tratta);
+                break;
+            case 6:
+                ricerca_tratta_dicotomica(rif_partenze);
+                break;
+            default:
                 break;
         }
     }
     while(choice!=0);
+}
+
+void ordina_vett_pointer_by_tratta(struct corse **rif_tratta){
+    int i, j, l=0, r=n_corse-1, flag = 1;
+    struct corse *temp;
+    for(i = l;i<r && flag==1;i++){
+        flag = 0;
+        for(j = l; j < r - i + l; j++){
+            if(strcmp(rif_tratta[j]->cod_tratta,rif_tratta[j+1]->cod_tratta)>0){
+                flag = 1;
+                temp = rif_tratta[j];
+                rif_tratta[j] = rif_tratta[j+1];
+                rif_tratta[j+1] = temp;
+            }
+        }
+    }
+    printf("Vettore di puntatori ordinato con successo in ordine di codice di tratta\n");
 }
 
 void ordina_vett_pointer_by_data(struct corse **rif_data){
@@ -80,11 +107,7 @@ void ordina_vett_pointer_by_data(struct corse **rif_data){
             }
         }
     }
-
-    for(i=0;i<n_corse;i++){
-        printf("%s %s %s %s %s %s %d\n",rif_data[i]->cod_tratta,rif_data[i]->partenza,
-        rif_data[i]->destinazione,rif_data[i]->data,rif_data[i]->ora_part,rif_data[i]->ora_arr,rif_data[i]->ritardo);
-    }
+    printf("Vettore di puntatori ordinato con successo in ordine di data\n");
 
 }
 
@@ -102,10 +125,7 @@ void ordina_vett_pointer_by_partenza(struct corse **rif_part){
             }
         }
     }
-    for(i=0;i<n_corse;i++){
-        printf("%s %s %s %s %s %s %d\n",rif_part[i]->cod_tratta,rif_part[i]->partenza,
-        rif_part[i]->destinazione,rif_part[i]->data,rif_part[i]->ora_part,rif_part[i]->ora_arr,rif_part[i]->ritardo);
-    }
+    printf("Vettore di puntatori ordinato con successo in ordine di stazione di partenza\n");
 }
 
 void ordina_vett_pointer_by_arrivo(struct corse **rif_arr){
@@ -122,29 +142,50 @@ void ordina_vett_pointer_by_arrivo(struct corse **rif_arr){
             }
         }
     }
-
-    for(i=0;i<n_corse;i++){
-        printf("%s %s %s %s %s %s %d\n",rif_arr[i]->cod_tratta,rif_arr[i]->partenza,
-        rif_arr[i]->destinazione,rif_arr[i]->data,rif_arr[i]->ora_part,rif_arr[i]->ora_arr,rif_arr[i]->ritardo);
-    }
-
+    printf("Vettore di puntatori ordinato con successo in ordine di stazione di arrivo\n");
 }
 
 void ricerca_tratta_lineare(struct corse *lc){
     char partenza[LENMAX];
-    int flag;
     printf("Inserisci la stazione da cercare:\n");
     scanf("%s",partenza);
     if(islower(partenza[0]))
         partenza[0] = partenza[0] - 32;
     for(int i=0;i<n_corse;i++){
-        flag = 1;
-        for(int j=0;j< strlen(partenza);j++){
-            if(partenza[j]!=(lc[i].partenza)[j])
-                flag = 0;
-        }
-        if(flag==1)
+        if(strncmp(lc[i].partenza,partenza, strlen(partenza))==0)
             printf("%s %s %s %s %s %s %d\n",lc[i].cod_tratta,lc[i].partenza,lc[i].destinazione,lc[i].data,lc[i].ora_part,lc[i].ora_arr,lc[i].ritardo);
+    }
+}
+
+void ricerca_tratta_dicotomica(struct corse **rif_part) {
+    char partenza[LENMAX];
+    int m,k,end=0;
+    printf("Inserisci la tratta da cercare: \n");
+    scanf("%s", partenza);
+    if(islower(partenza[0]))
+        partenza[0] = partenza[0] - ('a' - 'A');
+    ordina_vett_pointer_by_partenza(rif_part);
+    int start_index = 0, end_index = n_corse - 1;
+    while (start_index <= end_index && end==0) {
+        m = start_index + (end_index - start_index) / 2;
+        if (strncmp(rif_part[m]->partenza, partenza, strlen(partenza)) == 0) {
+            printf("%s %s %s %s %s %s %d\n", rif_part[m]->cod_tratta, rif_part[m]->partenza, rif_part[m]->destinazione, rif_part[m]->data,rif_part[m]->ora_part, rif_part[m]->ora_arr, rif_part[m]->ritardo);
+            k = m + 1;
+            while(k<n_corse && strncmp(rif_part[k]->partenza, partenza, strlen(partenza)) == 0){
+                printf("%s %s %s %s %s %s %d\n", rif_part[k]->cod_tratta, rif_part[k]->partenza, rif_part[k]->destinazione, rif_part[k]->data, rif_part[k]->ora_part, rif_part[k]->ora_arr, rif_part[k]->ritardo);
+                k++;
+            }
+            k = m - 1;
+            while(k>0 && strncmp(rif_part[k]->partenza, partenza, strlen(partenza)) == 0){
+                printf("%s %s %s %s %s %s %d\n", rif_part[k]->cod_tratta, rif_part[k]->partenza, rif_part[k]->destinazione, rif_part[k]->data, rif_part[k]->ora_part, rif_part[k]->ora_arr, rif_part[k]->ritardo);
+                k--;
+            }
+            end = 1;
+        }
+        else if (strcmp(rif_part[m]->partenza, partenza) < 0)
+            start_index = m + 1;
+        else
+            end_index = m - 1;
     }
 }
 
