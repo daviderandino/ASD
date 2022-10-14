640764d56e60d8a869870289eb8dcc1651a5c8cd
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-10-14 11:22:00 +0200

    Update Es1.c

--- Lab 2/Es1.c
+++ Lab 2/Es1.c
@@ -3,21 +3,16 @@
 int gcd(int a, int b);
 
 int main(){
-    int a=376,b=24,mcd;
+    int a=1639238420,b=1324701,mcd;
     mcd = gcd(a,b);
     printf("%d\n",mcd);
 }
 
 int gcd(int a, int b){
-    int tmp;
-    if(b>a){
-        tmp = a;
-        a = b;
-        b = tmp;
-    }
-    if(b==a) return a;
-    else if(a%2==0 && b%2==0) return 2* gcd(a/2,b/2);
-    else if(a%2==1 && b%2==0) return gcd(a,b/2);
-    else if(a%2==0 && b%2==1) return gcd(a/2,b);
-    else if(a%2==1 && b%2==1) return gcd((a-b)/2,b);
+    if(b>a) return gcd(b,a);
+    if(a==b) return a;
+    if(a%2==0 && b%2==0) return 2* gcd(a/2,b/2);
+    if(a%2==1 && b%2==0) return gcd(a,b/2);
+    if(a%2==0 && b%2==1) return gcd(a/2,b);
+    if(a%2==1 && b%2==1) return gcd((a-b)/2,b);
 }
