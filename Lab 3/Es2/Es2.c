4105d218933592591f688662c9372a09065304ae
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-10-18 22:49:51 +0200

    Update Es2.c

--- Lab 3/Es2/Es2.c
+++ Lab 3/Es2/Es2.c
@@ -14,6 +14,7 @@ int princ_molt(int pos, Livello *val, char **sol, int n, int cnt);
 int main(){
     char *file_name = "../brani.txt";
     leggi_file(file_name);
+    return 0;
 }
 
 void leggi_file(char *file_name){
@@ -23,30 +24,33 @@ void leggi_file(char *file_name){
     char **sol,n;
     int totali;
     fscanf(fin,"%d",&n);
+    // costruzione del vettore di struct "val"
     val = (Livello *) malloc(n*sizeof(Livello));
-    sol=(char **) malloc(n* sizeof(char *));
-    for(int i=0;i<n;i++)
-        sol[i]=(char *) malloc(MAXCH); // sizeof(char) = 1 byte
-    for(int i=0;i<n;i++){
-        fscanf(fin,"%d",&val[i].num_scelte);
-        val[i].scelte = (char **) malloc(val[i].num_scelte*sizeof(char *));
-        for(int j=0;j<val[i].num_scelte;j++){
-            (val[i].scelte)[j]=(char *) malloc(MAXCH);
-            fscanf(fin,"%s",(val[i].scelte)[j]);
+    for(int i=0;i<n;i++) {
+        fscanf(fin, "%d", &val[i].num_scelte);
+        val[i].scelte = (char **) malloc(val[i].num_scelte * sizeof(char *));
+        for (int j = 0; j < val[i].num_scelte; j++) {
+            (val[i].scelte)[j] = (char *) malloc(MAXCH);
+            fscanf(fin, "%s", (val[i].scelte)[j]);
         }
     }
+    // sol è il vettore delle soluzioni: avrà dimensione n (come il numero di amici poichè viene scelta una canzone a testa)
+    sol=(char **) malloc(n* sizeof(char *));
+    for(int i=0;i<n;i++)
+        sol[i]=(char *) malloc(MAXCH); // sizeof(char) = 1 byte, si può omettere
 
+    // dopo aver costruito il vettore di struct "val" si procede chiamando la funzione ricorsiva princ_molt
     totali = princ_molt(0,val,sol,n,0);
     printf("%d playlist possibili",totali);
     fclose(fin);
 }
 
 int princ_molt(int pos, Livello *val, char **sol, int n, int cnt){
-    if(pos>=n){
+    if(pos>=n){ // caso terminale: la dimensione della soluzione è >= di quella richiesta. Può stampare gli elementi di sol.
         for(int i=0;i<n;i++)
             printf("%s ",sol[i]);
         printf("\n");
-        return cnt+1;
+        return cnt+1; // cnt conta le soluzioni trovate: 1 in più
     }
     for(int i=0;i<val[pos].num_scelte;i++){
         strcpy(sol[pos],(val[pos].scelte)[i]);
