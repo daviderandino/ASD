4ee743b5d40da863c2ee66583940f05ebea582af
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-10-19 11:04:20 +0200

    Update merge_sort.c

--- Ricorsione/merge_sort.c
+++ Ricorsione/merge_sort.c
@@ -14,31 +14,31 @@ int main(){
 }
 
 void MergeSort(int A[],int B[],int N){
-    int l=0,r=N-1; //indici
+    int l=0,r=N-1;
     MergeSortR(A,B,l,r);
 }
 
 void MergeSortR(int A[],int B[],int l,int r){
-    int q=(l+r)/2; //indice di mezzo, in questo modo definsico vettore di sx e dx
-    if(l>=r) return; //se l'indice sx supera o raggiunge quello destro mi fermo
-    MergeSortR(A,B,l,q);//mergesort sul sottovettore sx
-    MergeSortR(A,B,q+1,r);//mergesort sul sottovettore dx
-    Merge(A,B,l,q,r);//fusione
+    int q=(l+r)/2;
+    if(l>=r) return; 
+    MergeSortR(A,B,l,q);
+    MergeSortR(A,B,q+1,r);
+    Merge(A,B,l,q,r);
 }
 
 void Merge(int A[],int B[],int l,int q,int r){
     int i,j,k;
-    i=l; //inizio sottovettore sx
-    j=q+1; //inizio sottovettore dx
+    i=l;
+    j=q+1;
 
     for(k=l;k<=r;k++){
-        if(i>q){//sottovettore sx terminato, uso l indice del sottovettore dx e riempo sottovettore dx
+        if(i>q){
             B[k]=A[j++];
-        }else if(j>r){//sottovettore dx terminato e riempo il sottovettore sx, uso l'indice del sottovettore sx
+        }else if(j>r){
             B[k]=A[i++];
-        }else if(A[j]>A[i]){ //se a[j]>[i] ricopiare a[i] in B e avanzare i (j resta invariato)
+        }else if(A[j]>A[i]){ 
           B[k]=A[i++];
-        }else{ //altrimenti ricopiare a[j] in b e avanzare j, i resta invariato
+        }else{
             B[k]=A[j++];
         }
     }
