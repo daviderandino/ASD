852edee46a050c5198bad1eb62ee0f63c8e5d6b1
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-11-08 15:21:44 +0100

    Update Es1.c

--- Lab 5/Es1.c
+++ Lab 5/Es1.c
@@ -1,39 +1,38 @@
-/*
-Da completare modularizzando bene e aggiungendo funzione wrapper
-*/
-
-
 #include<stdio.h>
 #include<stdlib.h>
 
 struct activity{
     int inizio,fine;
-};
+} typedef att;
+
+void attSel(int N, att *v);
 
 int powerset(int pos,struct activity *val,int *sol,int n,int *cnt,int *el,struct activity *max,int *len_max);
 
 int main(){
-    int num_activities,pos=0,cnt=0,el,len_max=-1;
-    struct activity *activities;
-    FILE *fin = fopen("../es1input.txt","r");
-
-    fscanf(fin,"%d",&num_activities);
+    int num_activities=6;
+    att *activities;
     activities = (struct activity *) malloc(sizeof(struct activity)*(num_activities));
-    for(int i=0;i<num_activities;i++){
-        fscanf(fin,"%d",&activities[i].inizio);
-        fscanf(fin,"%d",&activities[i].fine);
-    }
-    int *sol = (int*) malloc(num_activities*sizeof(int));
-    struct activity *max = (struct activity *) malloc(sizeof(struct activity)*(num_activities));
-    powerset(pos,activities,sol,num_activities,&cnt,&el,max,&len_max);
+    attSel(num_activities,activities);
+}
 
+void attSel(int N, att *v){
+    int pos=0,cnt=0,el,len_max=-1;
+    FILE *fin = fopen("../es1input.txt","r");
+    for(int i=0;i<N;i++){
+        fscanf(fin,"%d",&v[i].inizio);
+        fscanf(fin,"%d",&v[i].fine);
+    }
+    fclose(fin);
+    int *sol = (int*) malloc(N*sizeof(int));
+    att *max = (att *) malloc(sizeof(struct activity)*(N));
+    powerset(pos,v,sol,N,&cnt,&el,max,&len_max);
     printf("Un insieme di attivita' che non si intersecano e che massimizza la durata e':\n");
     for(int i=0;i<=el;i++)
         printf("(%d,%d) ",max[i].inizio,max[i].fine);
-
 }
 
-int powerset(int pos,struct activity *val,int *sol,int n,int *cnt,int *el,struct activity *max,int *len_max){
+int powerset(int pos,att *val,int *sol,int n,int *cnt,int *el,struct activity *max,int *len_max){
     int j,intersection,count,len;
     if(pos>=n){
         intersection = 0; len = 0;
