f1c0f42813fcd4ea8bd7a3a7dd135ec9411e63bf
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2022-11-14 09:58:34 +0100

    Update Es2.c

--- Lab 5/Es2/Es2.c
+++ Lab 5/Es2/Es2.c
@@ -25,89 +25,105 @@ struct tiles{
     int val1,val2,rotazione;
 } typedef t;
 
-int disp(int pos,int *val,int *sol,int *mark, int n, int k,int cnt,int nr,int nc,int **Board,int *indici_occup);
+int disp(int pos,int *val,int *sol,int *mark, int n, int k,int cnt,int nr,int nc,int **Board,int *indici_liberi,int n_ind_liberi,int *p_max,t *tess_tot);
 t *alloca_tessere(t *Vett,int *N);
 int **alloca_board(int **Board, int *nr, int *nc,t *Vett);
-void cerca_tessere_disponibili(int *tess_disp,int **Board,int nr,int nc,int *n,int N,int *indici_occup);
-void stampa_indici_occupati_e_tessere_disponibili(int *indici_occup,int *tess_disp,int n_occupate,int n_disp);
-void dealloca_tutto(t *tess_tot,int *tess_disp,int *indici_occup,int **Board,int nr);
+void cerca_tessere_disponibili(int *tess_disp,int **Board,int nr,int nc,int *n,int N,int *indici_liberi);
+void stampa_indici_liberi_e_tessere_disponibili(int *indici_liberi,int *tess_disp,int n_ind_liberi,int n_disp);
+void dealloca_tutto(t *tess_tot,int *tess_disp,int *indici_liberi,int **Board,int nr);
+int calcola_punteggio(int **Board,int nr,int nc,t *tess_tot);
 
 int main(){
     t *tess_tot;
-    int **Board,*tess_disp,*indici_occup;
-    int N_tot,nr,nc,n_disp,pos=0,cnt=0,n_occupate;
+    int **Board,*tess_disp,*indici_liberi;
+    int N_tot,nr,nc,n_disp,pos=0,cnt=0,n_ind_liberi;
+    int punteggio_max;
     tess_tot = alloca_tessere(tess_tot,&N_tot);
     tess_disp = (int *) malloc(N_tot*sizeof(int));
-    indici_occup = (int *) malloc(N_tot*sizeof(int));
+    indici_liberi = (int *) malloc(N_tot*sizeof(int));
     Board = alloca_board(Board,&nr,&nc,tess_tot);
-    cerca_tessere_disponibili(tess_disp,Board,nr,nc,&n_disp,N_tot,indici_occup);
-    n_occupate = N_tot - n_disp;
-    // stampa_indici_occupati_e_tessere_disponibili(indici_occup,tess_disp,n_occupate,n_disp);
+    cerca_tessere_disponibili(tess_disp,Board,nr,nc,&n_disp,N_tot,indici_liberi);
+    n_ind_liberi = N_tot - n_disp + 1;
+    stampa_indici_liberi_e_tessere_disponibili(indici_liberi,tess_disp,n_ind_liberi,n_disp);
     int *sol = (int*) malloc(n_disp*sizeof(int));
     int *mark = (int*) malloc(n_disp*sizeof(int));
     for(int i=0;i<n_disp;i++) mark[i] = 0;
-    cnt = disp(pos,tess_disp,sol,mark,n_disp,n_disp,cnt,nr,nc,Board,indici_occup);
-    dealloca_tutto(tess_tot,tess_disp,indici_occup,Board,nr);
+    cnt = disp(pos,tess_disp,sol,mark,n_disp,n_disp,cnt,nr,nc,Board,indici_liberi,n_ind_liberi,&punteggio_max,tess_tot);
+    dealloca_tutto(tess_tot,tess_disp,indici_liberi,Board,nr);
 }
 
-int disp(int pos,int *val,int *sol,int *mark, int n, int k,int cnt,int nr,int nc,int **Board,int *indici_occup){
+int disp(int pos,int *val,int *sol,int *mark, int n, int k,int cnt,int nr,int nc,int **Board,int *indici_liberi,int n_ind_liberi,int *p_max, t *tess_tot){
+    int punteggio;
     if (pos >= k){
-        for (int l=0; l<k; l++){
-            printf("%d ", sol[l]);
-            for(int i=0;i<nr;i++){
-                for(int j=0;j<nc;j++){
-
-                    // if(Board[i][j]==-1) Board[i][j] = sol[l]; // modificare perchè Board[i][j] sarà diverso da -1 dopo la prima soluzione
-                    // quindi serve un vettore che tiene conto dei posti vuoti all'inizio
+        for(int i=0;i<n_ind_liberi;i++) {
+            for (int j = 0; j < nr; j++) {
+                for (int l = 0; l < nc; l++) {
+                    if (nc * j + l == indici_liberi[i]){
+                        Board[j][l] = sol[i];
+                    }
                 }
             }
         }
 
+    punteggio = calcola_punteggio(Board,nr,nc,tess_tot);
+    if(punteggio>*p_max) *p_max = punteggio;
+
+    for(int i=0;i<nr;i++){
+        for(int j=0;j<nc;j++){
+            printf("%d ",Board[i][j]);
+        }
         printf("\n");
-        return cnt+1;
+    }
+    printf("\n");
+
+
+    return cnt+1;
     }
     for (int i=0; i<n; i++){
         if (mark[i] == 0) {
             mark[i] = 1;
             sol[pos] = val[i];
-            cnt = disp(pos+1,val,sol,mark,n,k,cnt,nr,nc,Board,indici_occup);
+            cnt = disp(pos+1,val,sol,mark,n,k,cnt,nr,nc,Board,indici_liberi,n_ind_liberi,p_max,tess_tot);
             mark[i] = 0;
         }
     }
     return cnt;
 }
 
-void dealloca_tutto(t *tess_tot,int *tess_disp,int *indici_occup,int **Board,int nr){
+int calcola_punteggio(int **Board,int nr,int nc,t *tess_tot){
+    
+}
+
+void dealloca_tutto(t *tess_tot,int *tess_disp,int *indici_liberi,int **Board,int nr){
     for(int i=0;i<nr;i++)
         free(Board[i]);
     free(Board);
     free(tess_tot);
     free(tess_disp);
-    free(indici_occup);
+    free(indici_liberi);
 }
 
-void stampa_indici_occupati_e_tessere_disponibili(int *indici_occup,int *tess_disp,int n_occupate,int n_disp){
-    printf("Indici occupati:\n");
-    for(int i=0;i<n_occupate;i++){
-        printf("%d ",indici_occup[i]);
+void stampa_indici_liberi_e_tessere_disponibili(int *indici_liberi,int *tess_disp,int n_ind_liberi,int n_disp){
+    printf("Indici liberi:\n");
+    for(int i=0;i<n_ind_liberi;i++){
+        printf("%d ",indici_liberi[i]);
     }
     printf("\n");
 
     printf("Tessere libere:\n");
     for(int i=0;i<n_disp;i++)
         printf("%d ",tess_disp[i]);
-    printf("\n");
+    printf("\n\n");
 }
 
-void cerca_tessere_disponibili(int *tess_disp,int **Board,int nr,int nc,int *n,int N,int *indici_occup){
+void cerca_tessere_disponibili(int *tess_disp,int **Board,int nr,int nc,int *n,int N,int *indici_liberi){
     int cnt=0,cnt2=0,flag,cnt3=0;
     int *V = (int*) malloc(N*sizeof(int));
     for(int i=0;i<nr;i++) {
         for (int j = 0; j < nc; j++) {
             if (Board[i][j] != -1){
                 V[cnt++] = Board[i][j];
-                indici_occup[cnt3++] = nc*i+j;
-            }
+            } else indici_liberi[cnt3++] = nc*i+j;
         }
     }
 
